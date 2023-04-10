d59fa9a4adc36ce85da790e951543f04d30642bf
NitroAS <98990207+NitroAS@users.noreply.github.com>
2023-04-09 23:49:22 -0300

    feat:  first commit

---
+++ src/tiposVariaveis/TiposVariaveis.java
@@ -0,0 +1,23 @@
+package tiposVariaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        // Tipos primitivos
+        // Estudar  a class string  que representa  texto na aplicação
+        double salarioMinimo = 1200;
+        String meuNome = "Alexsandro";
+        short numeroCurto = 1;
+
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+    }
+}
